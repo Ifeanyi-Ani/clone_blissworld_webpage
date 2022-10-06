4c48f6252a38c5d460095f5cf8ec5a542d82dc9c
Ifeanyi Ani <ifeanyiani155@gmail.com>
2022-10-06 07:03:22 +0100

    admin adding more pages

--- js/animation.js
+++ js/animation.js
@@ -1,8 +1,21 @@
+
+$(document).ready(function(){
+  $("[data-toggle='dropdown']").click(function(e) {   
+      $(this).parents(".dropdown").toggleClass("active");  /*when you click on an element with attr data-toggle='dropdown' it toggle the class "open" on its parent with class "dropdown"*/
+      e.stopPropagation();
+  });
+
+  $("html").click(function() {
+      $(".active").removeClass("active");  /*when you click out of the dropdown-menu it remove the class "open"*/
+  });
+});
+
+
 const swiper = new Swiper('.swiper', {
-    // autoplay:{
-    //     delay:5000,
-    //     disableOnInteraction:false,
-    // },
+    autoplay:{
+        delay:5000,
+        disableOnInteraction:false,
+    },
     loop: true,
   
     // If we need pagination
