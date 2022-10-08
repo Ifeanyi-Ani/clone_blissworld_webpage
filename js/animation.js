41b63655f94b6d68cf94a286ed8e91902358d478
Ifeanyi Ani <ifeanyiani155@gmail.com>
2022-10-08 21:22:32 +0100

    admin

--- js/animation.js
+++ js/animation.js
@@ -1,14 +1,29 @@
 
-$(document).ready(function(){
-  $("[data-toggle='dropdown']").click(function(e) {   
-      $(this).parents(".dropdown").toggleClass("active");  /*when you click on an element with attr data-toggle='dropdown' it toggle the class "open" on its parent with class "dropdown"*/
-      e.stopPropagation();
-  });
+// $(document).ready(function(){
+//   $("[data-toggle='dropdown']").click(function(e) {   
+//       $(this).parents(".dropdown").toggleClass("active");  /*when you click on an element with attr data-toggle='dropdown' it toggle the class "open" on its parent with class "dropdown"*/
+//       e.stopPropagation();
+//   });
 
-  $("html").click(function() {
-      $(".active").removeClass("active");  /*when you click out of the dropdown-menu it remove the class "open"*/
-  });
-});
+//   $("html").click(function() {
+//       $(".active").removeClass("active");  /*when you click out of the dropdown-menu it remove the class "open"*/
+//   });
+// });
+
+document.addEventListener("click",e =>{
+  const isDropDown=e.target.matches("[data-toggle]");
+  if (!isDropDown && e.target.closest(".dropdown") !=null) return
+    let currentDropdown
+  if(isDropDown){
+    currentDropdown = e.target.closest(".dropdown")
+    currentDropdown.classList.toggle("active")
+  }
+  document.querySelectorAll(".dropdown").forEach(drop =>{
+    if(drop===currentDropdown) return
+    drop.classList.remove("active")
+  })
+ 
+})
 
 
 const swiper = new Swiper('.swiper', {
